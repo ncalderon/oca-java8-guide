914af950385b0d643aaa54405cd6911f94a5c3cf
Nathaniel Calderon <nathaniel.calderon@gmail.com>
2016-07-15 09:57:14 -0400

    Advancing Chapter about 5

---
+++ Practice/Chapter 5/Animal.java
@@ -0,0 +1,10 @@
+import pkg.Lion;
+
+class Animal {
+	public static void main (String ... args) {
+		System.out.println("Testing");
+		Lion lion2 = new Lion();
+		pkg.Lion lion = new pkg.Lion();
+		System.out.println("Was created");
+	}
+}
